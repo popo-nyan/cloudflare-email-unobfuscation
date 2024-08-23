93427feb3156345f33f63bc957ddeb11add08c3d
Kounomiya <kounomiya@tuta.io>
2024-08-23 03:40:00 +0000

    refactoring

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-def r(e: str, t: int) -> str:
+def r(e: str, t: int) -> int:
     r = e[t : 2 + t]
     return int(r, 16)
 
 
-def n(n: str, c: int) -> str:
+def n(n: str, c: int = 0) -> str:
     o = ""
     a = r(n, c)
     for i in range(2, len(n), 2):
         l = r(n, i) ^ a
-        o += str(chr(l))
+        o += chr(l)
     return o
 
 
 def main():
     obfuscated_email_address = "531032213f3c207d1b3c3f3426362132133c243220237d3c2134"
-    email_address = n(obfuscated_email_address, 0)
+    email_address = n(obfuscated_email_address)
     print(email_address)
 
 
--- poc.py
+++ poc.py
@@ -1,25 +1,23 @@
-import tls_client
+from tls_client import Session
 from bs4 import BeautifulSoup
 
 
-def r(e: str, t: int) -> str:
+def r(e: str, t: int) -> int:
     r = e[t : 2 + t]
     return int(r, 16)
 
 
-def n(n: str, c: int) -> str:
+def n(n: str, c: int = 0) -> str:
     o = ""
     a = r(n, c)
     for i in range(2, len(n), 2):
         l = r(n, i) ^ a
-        o += str(chr(l))
+        o += chr(l)
     return o
 
 
 def main():
-    session = tls_client.Session(
-        client_identifier="firefox_120", random_tls_extension_order=True
-    )
+    session = Session(client_identifier="firefox_120", random_tls_extension_order=True)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -42,14 +40,14 @@ def main():
 
     for data_cf_email in soup.find_all("a", {"class": "__cf_email__"}):
         obfuscated_email_address = data_cf_email.get("data-cfemail")
-        email_address = n(obfuscated_email_address, 0)
+        email_address = n(obfuscated_email_address)
         print(email_address)
 
     for cf_email in soup.find_all("a"):
         cf_email = cf_email.get("href")
         if "/cdn-cgi/l/email-protection#" in cf_email:
             obfuscated_email_address = cf_email.split("#")[1]
-            email_address = n(obfuscated_email_address, 0)
+            email_address = n(obfuscated_email_address)
             print(email_address)
 
 
